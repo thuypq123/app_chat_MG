88162ae75d8c13600c580feb36d5bbae7718aa5a
Lê Minh Thủy <40124962+thuypq123@users.noreply.github.com>
2021-09-26 08:20:06 +0700

    Update database.js

--- src/config/database.js
+++ src/config/database.js
@@ -4,10 +4,12 @@ const url1 = "mongodb+srv://socket_io:thuy1234@cluster0.uhkg4.mongodb.net/socket
 const url = "mongodb+srv://socket_io:thuy1234@cluster0.uhkg4.mongodb.net/socket_io?retryWrites=true&w=majority"
 
 //================================
-
+var options = { server: { socketOptions: { keepAlive: 300000, connectTimeoutMS: 30000 } }, 
+                replset: { socketOptions: { keepAlive: 300000, connectTimeoutMS : 30000 } } };
+//================================
 async function connect(){
     try {
-        await mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true });
+        await mongoose.connect(url, options);
         console.log('Success Connected');
     } catch (error) {
         console.log("error");
